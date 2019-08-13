c526e0864db31a929239bac17c931d4e18b8d585
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-08-13 15:14:25 -0500

    modified iterating arrays file

--- js/iterating.js
+++ js/iterating.js
@@ -6,7 +6,7 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ['Mark', 'Deb', 'Alex', 'Meghan'];
+    var names = ['Meghan', 'Mark', 'Deb', 'Alex'];
 
     /**
      * TODO:
@@ -20,17 +20,27 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+        console.log("This person's name is " + names[0]);
+        console.log("This person's name is " + names[1]);
+        console.log("This person's name is " + names[2]);
+        console.log("This person's name is " + names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    names.forEach(function(name) {
+        console.log(name);
+    })
 
     /**
      * TODO:
@@ -45,5 +55,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function first(arr) {
+        return arr[0];
+    }
 
+    function second(arr) {
+        return arr[1];
+    }
+
+    function last(arr) {
+        return arr[5];
+
+
+        console.log(first(names));
+        console.log(second(names));
+        console.log(last(names));
+    }
 })();
