7889889ee334f74ff4873423a617ad7ee5cdec3a
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-08-13 08:18:14 -0500

    modified for loops and while js file

---
+++ js/for-loops.js
@@ -0,0 +1,61 @@
+// function showMultiplicationTable(number) {
+//     for (var i = 1; i < 11; i++) {
+//         console.log(i + " x " + number + " = " + (i * number));
+//     }
+// }
+// showMultiplicationTable(7);
+//
+//
+// // for (var i = 1; i < 11; i++) {
+// //     console.log("7 x " + i + " = " + (7 * i));
+// // }
+//
+//
+// function randOddEven() {
+//     for (var i = 1; i <10; i++) {
+//         var number = Math.floor(Math.random() * 200 + 20);
+//         if (number % 2 === 0) {
+//             console.log(number +  " is even ");
+//         } else {
+//             console.log(number + " is odd ");
+//         }
+//     }
+// }
+// randOddEven();
+//
+//
+//
+//     for(var i = 1; i < 10; i++) {
+//         var num = "";
+//         for(var j = 0; j < i; j++) {
+//         num = num + i;
+//         }
+//         console.log(num);
+// }
+//
+//
+//
+// for (var i = 100; i >= 5; i = i-5) {
+//     console.log(i);
+// }
+
+
+// var number = Math.floor((Math.random()* 50) + 1);
+// console.log("Random odd number to skip is " + number);
+//
+// for(var i = 1; i < 100; i++) {
+//     if(i % 2 === 0) {
+//     continue;
+//     }
+//     if (number === i) {
+//         console.log("Yikes! skipping number " + i);
+//     } else {
+//         console.log("Here is an odd number: " + i);
+//     }
+//     if (i >= 49) {
+//         break;
+//     }
+// }
+
+
+('b' + 'a' + + 'a' + 'a').toLowerCase();
--- js/while.js
+++ js/while.js
@@ -5,15 +5,18 @@
 //     console.log(i);
 // }
 
-var totalCones = Math.floor(Math.random() * 50) + 50;
+// var totalCones = Math.floor(Math.random() * 50) + 50;
+//
+// do {
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     console.log(conesSold + "cones sold!");
+//     if (conesSold > totalCones) {
+//         console.log("I can't sell you " + conesSold + ", I only have " + totalCones + ".");
+//     }
+// } while (totalCones > 0)
 
-do {
-    var conesSold = Math.floor(Math.random() * 5) + 1;
-    console.log(conesSold + "cones sold!");
-    if (conesSold > totalCones) {
-        console.log("I can't sell you " + conesSold + ", I only have " + totalCones + ".");
-    }
-} while (totalCones > 0)
+
+
 // console.log("Yay! I sold them all!")
 
 
