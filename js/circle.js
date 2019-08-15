ecb11d4ad1731bc078d1c4433e3ecd424a0ae0f8
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-08-15 11:01:42 -0500

    modified circle js file and updated objects js file

---
+++ js/circle.js
@@ -0,0 +1,50 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            var area = Math.PI * Math.pow(this.radius, 2);
+
+            // var area;
+            // area = Math.PI * this.radius ^2;
+
+            // TODO: return the proper value
+            return area;
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            if(doRounding == true) {
+                Math.round(circle.getArea());
+            } else {
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+            return circle.getArea();
+            }
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    // log info about the circle
+    circle.radius = 5;
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
