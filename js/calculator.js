a8052c1ba3569bd3eed2f4c3bd4f46dd658bf4df
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-11-19 09:28:18 -0600

    started calculator project and added an add, subtract, and multiply function

---
+++ js/calculator.js
@@ -0,0 +1,17 @@
+function add (x, y) {
+    x = 0;
+    y = 0;
+    var z = x + y;
+}
+add();
+
+function subtract(x, y) {
+    x = 0;
+    y = 0;
+    var z = x - y;
+}
+subtract();
+
+function multiply(x, y) {
+    var z = x * y;
+}
---
+++ practice.js
@@ -0,0 +1,51 @@
+//given number is divisible by three
+// checks whether an input is divisible by
+// function isThree (num) {
+//     if (num % 3) {
+//         console.log(false);
+//         return false;
+//     } else {
+//         console.log(true);
+//         return true;
+//     }
+// }
+//
+//     isThree(4);
+//
+//
+// function fizzbuzz(input) {
+//     for (var i = 1; i <= 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 == 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 == 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// fizzbuzz(100);
+
+
+
+// ================== FARM ANIMAL FUNCTIONS ==================
+
+// chicken == two legs
+// cows == four legs
+// pigs == four legs
+
+// function threeInputs(chickies, moomoos, piggies) {
+//     //return total number of legs on farm.
+//     var cl = 2;
+//     var ml = 4;
+//     var pl = 4;
+//
+//     var allLegs =((cl * chickies) + (ml * moomoos) + (pl * piggies));
+//     console.log(allLegs);
+// }
+// threeInputs(2, 3, 5);
+
+// ================== Problem on a website ==================
