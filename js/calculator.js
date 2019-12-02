0498f6f2899444231a1fbabd9701b70bf5e40d27
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-12-01 20:10:27 -0600

    Created logarithm pi sine cosine and tangent functions.

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,4 @@
-var pi = 3.14159
+var pi = 3.14159;
 
 
 function isNotANumber() {
@@ -10,19 +10,19 @@ function isNotANumber() {
 function add (x, y) {
     x = 0;
     y = 0;
-    var z = x + y;
+    let z = x + y;
 }
 add();
 
 function subtract(x, y) {
     x = 0;
     y = 0;
-    var z = x - y;
+    let z = x - y;
 }
 subtract();
 
 function multiply(x, y) {
-    var z = x * y;
+    let z = x * y;
 }
 multiply();
 
@@ -30,9 +30,29 @@ function divide(x, y) {
     if (y === 0) {
         console.log("ERROR: Cannot divide by zero. Try again.");
     } else {
-        var z = x / y;
+        let z = x / y;
     }
 divide();
+
+function logarithm() {
+
+}
+
+function pi() {
+
+}
+
+function sine() {
+
+}
+
+function cosine() {
+
+}
+
+function tangent() {
+
+}
 
 
 }
